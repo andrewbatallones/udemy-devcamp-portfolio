175e829f21c96af0aae1190207bab3a27c43354f
Kelthagas <batallonesa@gmail.com>
2018-02-19 22:31:39 -0800

    lecture 23

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  # Encapsulate predefined rails routes
+  # Take note that there are 2 diff types of update routes
+  # rake routes
+  # Prefix, routes will give predifined method calls so that we can use them "prefix_path"
   resources :blogs
-  
 end
